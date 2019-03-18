c55d32d5521e20386f6fd12cb29682ccd3f72423
venkateshrajb <venkateshrajetce@gmail.com>
2019-03-18 13:20:44 +0530

    successfully added

---
+++ src/org/greens/DayOne.java
@@ -0,0 +1,17 @@
+package org.greens;
+
+public class DayOne {
+
+	private void gitOne() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("open into git");
+
+	}
+	
+	public static void main(String[] args) {
+		DayOne gi = new DayOne();
+		
+		gi.gitOne();
+	}
+}
